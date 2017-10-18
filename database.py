62b3c1c03d776718bbe78369c20544c3fe2960ae
SquaredCanine <SquaredCanine@users.noreply.github.com>
2017-10-18 15:25:02 +0200

    Adding database functionality Using environment variables for the database data, and started a function to add journeys to the database.

--- database.py
+++ database.py
@@ -1,14 +1,27 @@
 import mysql.connector
+import os
 
-def test():
-    cnx = mysql.connector.connect(user='REDACTED', password='REDACTED', port='REDACTED', host='REDACTED', database='REDACTED')
+username = os.environ('db_username')
+location = os.environ('db_location')
+name = os.environ('db_name')
+port = os.environ('db_port')
+password = os.environ('db_password')
+global cnx, cursor
+cnx = None
+cursor = None
+
+
+def initialize():
+    global cnx, cursor
+    cnx = mysql.connector.connect(user=username, password=password, port=port, host=location, database=name)
     cursor = cnx.cursor()
-    query= ("SELECT * FROM mydb.Journey")
+
+
+def add_journey():
+    global cursor,cnx
+    query = ("SELECT * FROM mydb.Journey")
     cursor.execute(query)
     for element in cursor:
         print(element)
     cnx.close()
 
-
-if __name__ == '__main__':
-    test()
