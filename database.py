bb5bd5fe0c6db41dfd23b657a00136879c83c233
SquaredCanine <SquaredCanine@users.noreply.github.com>
2017-10-18 15:57:56 +0200

    Adding database functionality Can now get the max identifier for an UID, so you can save a journey on the correct identifier (without overwriting other stuff)

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import mysql.connector
 import os
 
-username = os.environ('db_username')
-location = os.environ('db_location')
-name = os.environ('db_name')
-port = os.environ('db_port')
-password = os.environ('db_password')
+username = os.environ['db_username']
+location = os.environ['db_location']
+name = os.environ['db_name']
+port = os.environ['db_port']
+password = os.environ['db_password']
 global cnx, cursor
 cnx = None
 cursor = None
@@ -19,12 +19,12 @@ def initialize():
 
 def get_journey_identifier(UID):
     global cursor, cnx
-    query = ("SELECT max(identifier) FROM Journey WHERE UID = %s")
-    cursor.execute(query, (UID))
+    query = "SELECT max(identifier) FROM Journey WHERE UID = %s"
+    cursor.execute(query, (UID, ))
     for element in cursor:
-        print(element)
+        print(element[0])
     cnx.close
-    return element
+    return element[0]
 
 
 def add_journey():
@@ -34,4 +34,3 @@ def add_journey():
     for element in cursor:
         print(element)
     cnx.close()
-
