7bcf38ca53b7336eeb6ef889dec85cede492ad24
SquaredCanine <SquaredCanine@users.noreply.github.com>
2017-10-25 15:16:18 +0200

    Adding functionality spelling error corrected

--- amazon.py
+++ amazon.py
@@ -3,8 +3,8 @@ import requests
 base_url = 'https://api.amazon.com/user/profile?access_token='
 
 
-def get_user_data(acces_token):
-    full_url = '{0}{1}'.format(base_url, acces_token)
+def get_user_data(access_token):
+    full_url = '{0}{1}'.format(base_url, access_token)
     print(full_url)
     response = requests.get(full_url)
     return response.json()
